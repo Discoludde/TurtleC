316a9cb993e8217a435046efcf06c411b5b95076
Ludvig Modin <modin.ludvig@hotmail.com>
2026-01-11 14:47:09 +0100

    Endast raka linjer

---
+++ TurtleC.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define WIDTH 30
+#define HEIGHT 30
+#define PI 3.14159265358979323846
+
+typedef struct turtle turtle;
+struct turtle{
+    char*** matrix;
+    int xpos;
+    int ypos;
+    double facing;
+};
+
+
+/*Turtle initilizing at pos x,y facing north defined as 0 deg*/
+turtle* init(int x,int y)
+{
+    turtle* t;
+    t = calloc(1,sizeof(turtle));
+    t->xpos = x;
+    t->ypos = y;
+    t->facing = 0;
+    t->matrix = malloc((HEIGHT+1)*sizeof(char**));
+    for(int row = 0;row<=HEIGHT;row++){
+        t->matrix[row] = malloc((WIDTH+1)*sizeof(char*));
+        for(int col = 0;col<=WIDTH;col++)
+            t->matrix[row][col] = " ";
+    }
+    return t;
+}
+
+void rotate(turtle* t,double deg)
+{
+    t->facing += deg;
+}
+
+void freeturtle(turtle* t)
+{
+    for(int i = 0;i<=HEIGHT;i++)
+        free(t->matrix[i]);
+    
+    free(t->matrix);
+    free(t);
+}
+
+void putbotline(turtle* t,int x, int y)
+{
+    t->matrix[y][x] = "-";
+}
+
+void putvertline(turtle* t,int x, int y)
+{
+    t->matrix[y][x] = "|";
+}
+
+void putwhorizontalline(turtle* t, int x,int y)
+{
+    t->matrix[y][x] = "\\";
+}
+
+void putehorizontalline(turtle* t, int x,int y)
+{
+    t->matrix[y][x] = "/";
+}
+
+void drawline(turtle* t,int length)
+{
+    int x0 = t->xpos;
+    int y0 = t->ypos;
+    int deltax = length*sin(t->facing*PI/180);
+    int deltay = length*cos(t->facing*PI/180);
+    int x1 = x0 + deltax;
+    int y1 = y0 + deltay;
+    if(deltay == 0){
+        int step = (deltax <0) ? -1: 1;
+        for(int x = x0;x != x1;x += step)
+            putbotline(t,x,y0);
+        
+        t->xpos = x1;
+        return;
+    }
+    if(deltax == 0){
+        int step = (deltay <0) ? -1: 1;
+        for(int y = y0;y != y1;y += step)
+            putvertline(t,x0,y);
+
+        t->ypos = y1;
+        return;
+    }
+}
+
+void draw(turtle* t)
+{
+    for(int i = HEIGHT;i>=0;i--){
+        for(int k = 0;k<=WIDTH;k++)
+            printf("%s",t->matrix[i][k]);
+        
+        printf("\n");
+    }
+}
+void main()
+{
+    turtle* t1 = init(10,10);
+    drawline(t1,10);
+    rotate(t1,90);
+    drawline(t1,5);
+    rotate(t1,-90);
+    drawline(t1,5);
+    draw(t1);
+    freeturtle(t1);
+}
